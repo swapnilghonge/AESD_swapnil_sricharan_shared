2c5b45697f93462dd0cb913494f18d95ef48c79b
swapnilghonge <swapnil.ghonge@gmail.com>
2022-12-04 21:13:29 -0700

    Chnages in tmp.c

--- tmp/tmp.c
+++ tmp/tmp.c
@@ -4,34 +4,22 @@
 #include<sys/ioctl.h>
 #include<fcntl.h>
 #include<unistd.h>
-#include<time.h>
 
 int main()
 {
-
-	time_t rawtime;
-	struct tm* timeinfo;
-	
-	
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
-	
-	
-	
-	//open file to read data on the i2c bus
 	int file;
-	char *bus = "/dev/i2c-2";
+	char *bus = "/dev/i2c-1";
 
 	if((file = open(bus, O_RDWR)) < 0)
 	{
-		printf("FAILED to read bus\n");
+		printf("error to read bus\n");
 		exit(1);
 	}
 	
-	//get the i2c device, TMP102 I2C address is 0x48
+	
 	ioctl(file, I2C_SLAVE, 0x48);
 
-	//send temperature regiser command
+
 	char config[1] = {0};
 	config[0] = 0x00;
 	write(file, config, 1);
@@ -39,26 +27,25 @@ int main()
 	
 	
 
-	float temp, final_temp, fahrenheit;
+	int temp, final_temp;
 	
 	unsigned char read_data[2] = {0};
 	
-	//read 2 bytes of temperature data
+	
 	if(read(file, read_data, 2)!=2)
 	{
-		printf("Error: Could not read byte\n");
+		printf("Error in dataread\n");
 	}
 	else
 	{
-		temp = ((read_data[0] << 4 ) | ( read_data[1] >> 4)); //convert data to 12 bit temperature values
+		temp = ((read_data[0] << 4 ) | ( read_data[1] >> 4)); 
 	}
 	
 	
-	final_temp = temp * 0.0625; //final temperature values
-	fahrenheit = (1.8 * final_temp) + 32;
+	final_temp = temp * 0.0625; 
 	
-	printf("The temperature in celsius %fC %s\r\n", final_temp, asctime(timeinfo)); //print C temperature data to terminal
-	printf("The temperature in fahrenheit %fF %s\r\n", fahrenheit, asctime(timeinfo)); //print F temperature to terminal
+	printf("temperature in celsius %dC", final_temp ); 
+	
 	return 0;
 
 }
