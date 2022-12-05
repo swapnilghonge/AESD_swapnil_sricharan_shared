0820671c8be22996d276cff64a73fbfed08b12fe
Sricharan Kidambi <srki3050@colorado.edu>
2022-12-05 15:53:52 -0700

    Modifying BME280 for only humidity

--- bme/bme.c
+++ bme/bme.c
@@ -32,58 +32,8 @@ int main()
 	// temp coefficents
 	int dig_T1 = (b1[0] + b1[1] * 256);
 	int dig_T2 = (b1[2] + b1[3] * 256);
-	if(dig_T2 > 32767)
-	{
-		dig_T2 -= 65536;
-	}
+	dig_T2 = dig_T2 > 32767?dig_T2 - 65536:dig_T2;
 	int dig_T3 = (b1[4] + b1[5] * 256);
-	if(dig_T3 > 32767)
-	{
-		dig_T3 -= 65536;
-	}
-
-	// pressure coefficents
-	int dig_P1 = (b1[6] + b1[7] * 256);
-	int dig_P2 = (b1[8] + b1[9] * 256);
-	if(dig_P2 > 32767)
-	{
-		dig_P2 -= 65536;
-	}
-	int dig_P3 = (b1[10] + b1[11] * 256);
-	if(dig_P3 > 32767)
-	{
-		dig_P3 -= 65536;
-	}
-	int dig_P4 = (b1[12] + b1[13] * 256);
-	if(dig_P4 > 32767)
-	{
-		dig_P4 -= 65536;
-	}
-	int dig_P5 = (b1[14] + b1[15] * 256);
-	if(dig_P5 > 32767)
-	{
-	dig_P5 -= 65536;
-	}
-	int dig_P6 = (b1[16] + b1[17] * 256);
-	if(dig_P6 > 32767)
-	{
-		dig_P6 -= 65536;
-	}
-	int dig_P7 = (b1[18] + b1[19] * 256);
-	if(dig_P7 > 32767)
-	{
-		dig_P7 -= 65536;
-	}
-	int dig_P8 = (b1[20] + b1[21] * 256);
-	if(dig_P8 > 32767)
-	{
-	dig_P8 -= 65536;
-	}
-	int dig_P9 = (b1[22] + b1[23] * 256);
-	if(dig_P9 > 32767)
-	{
-		dig_P9 -= 65536;
-	}
 
 	// Read 1 byte of data from register(0xA1)
 	reg[0] = 0xA1;
@@ -100,27 +50,14 @@ int main()
 	// Convert the data
 	// humidity coefficents
 	int dig_H2 = (b1[0] + b1[1] * 256);
-	if(dig_H2 > 32767)
-	{
-		dig_H2 -= 65536;
-	}
 	int dig_H3 = b1[2] & 0xFF ;
+	dig_H2 = dig_H2 > 32767?dig_H2 - 65536:dig_H2;
 	int dig_H4 = (b1[3] * 16 + (b1[4] & 0xF));
-	if(dig_H4 > 32767)
-	{
-		dig_H4 -= 65536;
-	}
+	dig_H4 = dig_H4 > 32767?dig_H4 - 65536:dig_H4;
 	int dig_H5 = (b1[4] / 16) + (b1[5] * 16);
-	if(dig_H5 > 32767)
-	{
-		dig_H5 -= 65536;
-	}
+	dig_H5 = dig_H5 > 32767?dig_H5 - 65536:dig_H5;
 	int dig_H6 = b1[6];
-	if(dig_H6 > 127)
-	{
-		dig_H6 -= 256;
-	}
-
+	dig_H6 = dig_H6 > 127?dig_H6 - 256:dig_H6;
 	// Select control humidity register(0xF2)
 	// Humidity over sampling rate = 1(0x01)
 	char config[2] = {0};
@@ -155,38 +92,13 @@ int main()
 	float var2 = ((((float)adc_t) / 131072.0 - ((float)dig_T1) / 8192.0) *
 					(((float)adc_t)/131072.0 - ((float)dig_T1)/8192.0)) * ((float)dig_T3);
 	float t_fine = (long)(var1 + var2);
-	float cTemp = (var1 + var2) / 5120.0;
-	float fTemp = cTemp * 1.8 + 32;
-
-	// Pressure offset calculations
-	var1 = ((float)t_fine / 2.0) - 64000.0;
-	var2 = var1 * var1 * ((float)dig_P6) / 32768.0;
-	var2 = var2 + var1 * ((float)dig_P5) * 2.0;
-	var2 = (var2 / 4.0) + (((float)dig_P4) * 65536.0);
-	var1 = (((float) dig_P3) * var1 * var1 / 524288.0 + ((float) dig_P2) * var1) / 524288.0;
-	var1 = (1.0 + var1 / 32768.0) * ((float)dig_P1);
-	float p = 1048576.0 - (float)adc_p;
-	p = (p - (var2 / 4096.0)) * 6250.0 / var1;
-	var1 = ((float) dig_P9) * p * p / 2147483648.0;
-	var2 = p * ((float) dig_P8) / 32768.0;
-	float pressure = (p + (var1 + var2 + ((float)dig_P7)) / 16.0) / 100;
-
+	
 	// Humidity offset calculations
 	float var_H = (((float)t_fine) - 76800.0);
 	var_H = (adc_h - (dig_H4 * 64.0 + dig_H5 / 16384.0 * var_H)) * (dig_H2 / 65536.0 * (1.0 + dig_H6 / 67108864.0 * var_H * (1.0 + dig_H3 / 67108864.0 * var_H)));
 	float humidity = var_H * (1.0 -  dig_H1 * var_H / 524288.0);
-	if(humidity > 100.0)
-	{
-		humidity = 100.0;
-	}else
-	if(humidity < 0.0) 
-	{
-		humidity = 0.0;
-	}
-
+	humidity = humidity > 100.0?100.0:humidity;
+	humidity = humidity < 0.0?0.0:humidity;
 	// Output data to screen
-	printf("Temperature in Celsius : %.2f C \n", cTemp);
-	printf("Temperature in Fahrenheit : %.2f F \n", fTemp);
-	printf("Pressure : %.2f hPa \n", pressure);
 	printf("Relative Humidity : %.2f RH \n", humidity);
 }
