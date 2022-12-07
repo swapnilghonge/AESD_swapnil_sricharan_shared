1c11a227129337f7417199dada2112d790f7f13d
swapnilghonge <swapnil.ghonge@gmail.com>
2022-12-07 16:04:41 -0700

    Video Commit 3

--- combine/combine.c
+++ combine/combine.c
@@ -16,48 +16,6 @@ struct mq_attr attr;
 
 typedef union i2c_smbus_data i2c_data;
 
-void read_tmp()
-{
-int file;
-	char *bus = "/dev/i2c-1";
-
-	if((file = open(bus, O_RDWR)) < 0)
-	{
-		printf("error to read bus\n");
-		exit(1);
-	}
-	
-	
-	ioctl(file, I2C_SLAVE, 0x48);
-
-
-	char config[1] = {0};
-	config[0] = 0x00;
-	write(file, config, 1);
-	sleep(1);
-	
-	
-
-	int temp, final_temp;
-	
-	unsigned char read_data[2] = {0};
-	
-	
-	if(read(file, read_data, 2)!=2)
-	{
-		printf("Error in dataread\n");
-	}
-	else
-	{
-		temp = ((read_data[0] << 4 ) | ( read_data[1] >> 4)); 
-	}
-	
-	
-	final_temp = temp * 0.0625; 
-	
-	printf("temperature in celsius %dC\n", final_temp ); 
-}
-
 void read_bme()
 {
 	// Create I2C bus
@@ -159,6 +117,7 @@ void read_bme()
 
 int main()
 {
+	read_bme();
 	int file;
 	char *bus = "/dev/i2c-1";
 	if((file = open(bus, O_RDWR)) < 0) 
